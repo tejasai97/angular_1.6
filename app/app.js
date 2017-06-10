c40e1c640ab635be540dc95ee54bab3564ce5a12
Gadipally Sai Teja <teja.g14@gmail.com>
2017-06-10 15:15:01 +0530

    Modules

---
+++ app/app.js
@@ -0,0 +1,13 @@
+var myNinjaApp = angular.module('myNinjaApp',[]);
+
+//before app starts
+myNinjaApp().config(function(){
+
+});
+
+//when the app starts
+myNinjaApp.run(function(){
+
+});
+
+myNinjaApp.controller
