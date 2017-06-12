66fef66e974f1eb8dd370232fd21e988ba295f4d
Gadipally Sai Teja <teja.g14@gmail.com>
2017-06-12 11:48:39 +0530

    filters

--- app/app.js
+++ app/app.js
@@ -1,6 +1,27 @@
 var myNinjaApp = angular.module('myNinjaApp',[]);
 
 myNinjaApp.controller('NinjaController',['$scope',function($scope){
-  $scope.message = "hello world";
-  $scope.ninjas = ['yoshi','crystal','ryu','john'];
+
+  $scope.ninjas = [
+    {
+      name:"teja",
+      belt:"green",
+      rate: 50
+    },
+    {
+      name:"sai",
+      belt:"black",
+      rate: 30
+    },
+    {
+      name:"kumar",
+      belt:"yellow",
+      rate: 10
+    },
+    {
+      name:"sravan",
+      belt:"red",
+      rate: 150
+    }
+  ];
 }]);
