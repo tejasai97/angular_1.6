dc9c887795737ec9221c65a3f2c8e91a82f7e3bb
Gadipally Sai Teja <teja.g14@gmail.com>
2017-06-12 12:58:41 +0530

    ng-submit directive

--- app/app.js
+++ app/app.js
@@ -5,7 +5,20 @@ myNinjaApp.controller('NinjaController',['$scope',function($scope){
   $scope.removeNinja= function(ninja){
     var removedNinja = $scope.ninjas.indexOf(ninja);
     $scope.ninjas.splice(removedNinja, 1);
-  }
+  };
+
+  $scope.addNinja= function(){
+    $scope.ninjas.push({
+      name:$scope.newninja.name,
+      belt:$scope.newninja.belt,
+      rate:parseInt($scope.newninja.rate),
+      available:true
+    });
+
+    $scope.newninja.name = "";
+    $scope.newninja.belt = "";
+    $scope.newninja.rate = "";
+  };
 
   $scope.ninjas = [
     {
