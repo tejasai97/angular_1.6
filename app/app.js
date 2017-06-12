b83c46bf215cb378eb479ee6e8e2b2a7474d5975
Gadipally Sai Teja <teja.g14@gmail.com>
2017-06-12 12:30:52 +0530

    ng-click directive

--- app/app.js
+++ app/app.js
@@ -2,6 +2,11 @@ var myNinjaApp = angular.module('myNinjaApp',[]);
 
 myNinjaApp.controller('NinjaController',['$scope',function($scope){
 
+  $scope.removeNinja= function(ninja){
+    var removedNinja = $scope.ninjas.indexOf(ninja);
+    $scope.ninjas.splice(removedNinja, 1);
+  }
+
   $scope.ninjas = [
     {
       name:"teja",
@@ -19,13 +24,19 @@ myNinjaApp.controller('NinjaController',['$scope',function($scope){
       name:"kumar",
       belt:"yellow",
       rate: 10,
-      available:false
+      available:true
     },
     {
       name:"sravan",
       belt:"red",
       rate: 150,
       available:true
-    }
+    },
+    {
+      name:"rohan",
+      belt:"orange",
+      rate: 1500,
+      available:true
+    },
   ];
 }]);
